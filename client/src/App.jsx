50ef822f6a5935a467a99a34f32f86a63110a2fc
MuniirAhmadi <139943294+MuniirAhmadi@users.noreply.github.com>
2024-05-29 01:44:33 +0800

    Add new register page (/registercopy)
    - Add more inputs
    - Use tailwind for style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import Registercopy from "./pages/Registercopy";
 import Login from "./pages/Login";
 import axios from "axios";
 import { Toaster } from "react-hot-toast";
@@ -20,6 +21,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/registercopy" element={<Registercopy />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
