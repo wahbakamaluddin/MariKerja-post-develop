b36becadf1e8be4cf134b43cb0af6662b56f2ced
Wahba Kamaluddin Nurdin <63429284+wahbakamaluddin@users.noreply.github.com>
2024-07-01 11:41:09 +0800

    Revert "update1"

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,36 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Root from "/routes/root.jsx";
-import Home from "/routes/home.jsx";
-import Profile from "/routes/profile.jsx";
-import Activity from "/routes/activity.jsx";
 import App from "./App.jsx";
 import "./index.css";
-import {
-  BrowserRouter as Router,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "/home",
-        element: <Home />,
-      },
-      {
-        path: "/activity",
-        element: <Activity />,
-      },
-    ],
-  },
-]);
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
